dfd0f5d83ff34a77f692f4bebeed74c437762552
user2 <user2@KH_C>
2019-08-28 20:29:31 +0900

    MainFrame 추가

--- src/inSeongJo/aHenIntoTheWild/view/MainFrame.java
+++ src/inSeongJo/aHenIntoTheWild/view/MainFrame.java
@@ -6,7 +6,8 @@ public class MainFrame extends JFrame{
 	public MainFrame() {
 		this.setSize(1024, 768);
 		
-		this.add(new Stage01());
+		this.add(new Stage01()); // new Stage01을 자신의 패널을 넣어서 실험
+		
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
