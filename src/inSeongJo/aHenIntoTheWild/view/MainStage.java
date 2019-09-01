a6900277625af9e47db461324222869cf83dd824
user2 <user2@KH_C>
2019-09-01 21:26:38 +0900

    기준 v_001 스테이지 4 생성

--- src/inSeongJo/aHenIntoTheWild/view/MainStage.java
+++ src/inSeongJo/aHenIntoTheWild/view/MainStage.java
@@ -2,18 +2,18 @@ package inSeongJo.aHenIntoTheWild.view;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import org.omg.PortableInterceptor.USER_EXCEPTION;
-
-import inSeongJo.aHenIntoTheWild.view.MainPage.MyMouseAdapter;
+import inSeongJo.aHenIntoTheWild.controller.UserManager;
 
 public class MainStage extends JPanel {
 	private MainFrame mf;
@@ -30,7 +30,7 @@ public class MainStage extends JPanel {
 
 		this.setLayout(null);
 		mf.add(this);
-		
+
 		//유저 버튼
 		Image userImage = new ImageIcon("images/YJimages/user2.png").getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
 		Image userPressedImage = new ImageIcon("images/YJimages/user2_pressed.png").getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
@@ -41,9 +41,9 @@ public class MainStage extends JPanel {
 		userButton.setBounds(20, 20, 70, 70);
 		userButton.setPressedIcon(new ImageIcon(userPressedImage));;
 		add(userButton);
-		
+
 		userButton.addMouseListener(new MyMouseAdapter());
-		
+
 		//저장 버튼
 		Image saveImage = new ImageIcon("images/YJimages/save.png").getImage().getScaledInstance(55, 55, Image.SCALE_SMOOTH);
 		Image savePressedImage = new ImageIcon("images/YJimages/save_pressed.png").getImage().getScaledInstance(55, 55, Image.SCALE_SMOOTH);
@@ -54,19 +54,19 @@ public class MainStage extends JPanel {
 		saveButton.setBounds(930, 20, 70, 70);
 		saveButton.setPressedIcon(new ImageIcon(savePressedImage));;
 		add(saveButton);
-		
+
 		//잎싹이 이미지
 		Image ipssak = new ImageIcon("images/YJimages/ipssak.png").getImage().getScaledInstance(180, 180, 0);
 		JLabel ipssakImage = new JLabel(new ImageIcon(ipssak));
 		ipssakImage.setBounds(800, 500, 200, 200);
 		add(ipssakImage);
-		
+
 		//잎싹이 버블텍스트
 		Image bubble = new ImageIcon("images/YJimages/bubbletext.png").getImage().getScaledInstance(850, 190, 0);
 		JLabel bubbleText = new JLabel(new ImageIcon(bubble));
 		bubbleText.setBounds(30, 500, 900, 200);
 		add(bubbleText);
-		
+
 		//STAGE1 버튼
 		Image stage1Image = new ImageIcon("images/YJimages/STAGE1.png").getImage().getScaledInstance(200, 200, 0);
 		JButton stage1Button = new JButton(new ImageIcon(stage1Image));
@@ -75,8 +75,8 @@ public class MainStage extends JPanel {
 		stage1Button.setFocusPainted(false);
 		stage1Button.setBounds(20, 200, 300, 300);
 		add(stage1Button);
-		
-		
+
+
 		//STAGE2 버튼
 		Image stage2Image = new ImageIcon("images/YJimages/STAGE1.png").getImage().getScaledInstance(200, 200, 0);
 		JButton stage2Button = new JButton(new ImageIcon(stage1Image));
@@ -85,7 +85,7 @@ public class MainStage extends JPanel {
 		stage2Button.setFocusPainted(false);
 		stage2Button.setBounds(250, 200, 300, 300);
 		add(stage2Button);
-		
+
 		//STAGE3 버튼
 		Image stage3Image = new ImageIcon("images/YJimages/STAGE1.png").getImage().getScaledInstance(200, 200, 0);
 		JButton stage3Button = new JButton(new ImageIcon(stage1Image));
@@ -94,7 +94,7 @@ public class MainStage extends JPanel {
 		stage3Button.setFocusPainted(false);
 		stage3Button.setBounds(470, 200, 300, 300);
 		add(stage3Button);
-		
+
 		//STAGE4 버튼
 		Image stage4Image = new ImageIcon("images/YJimages/STAGE1.png").getImage().getScaledInstance(200, 200, 0);
 		JButton stage4Button = new JButton(new ImageIcon(stage1Image));
@@ -103,31 +103,36 @@ public class MainStage extends JPanel {
 		stage4Button.setFocusPainted(false);
 		stage4Button.setBounds(710, 200, 300, 300);
 		add(stage4Button);
-		
-		
-		
-	}
 
-	public void paint(Graphics g) {
-		ScreenImage = createImage(1024, 768);
-		ScreenGraphics = ScreenImage.getGraphics();
-		screenDraw(ScreenGraphics);
-		g.drawImage(ScreenImage, 0, 0, null);
-	}
+		stage4Button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 
-	public void screenDraw(Graphics g) {
-		g.drawImage(background, 0, 0, null);
-		paintComponents(g);
-		this.repaint();
+				ChangePanel.changePanel(mf, MainStage, new Stage04(mf));
+			}
+		});
 	}
-	
-	
-	class MyMouseAdapter extends MouseAdapter{
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			System.out.println("클릭되었습니다.");
+		
+		public void paint(Graphics g) {
+			ScreenImage = createImage(1024, 768);
+			ScreenGraphics = ScreenImage.getGraphics();
+			screenDraw(ScreenGraphics);
+			g.drawImage(ScreenImage, 0, 0, null);
 		}
-	}
-	
+
+		public void screenDraw(Graphics g) {
+			g.drawImage(background, 0, 0, null);
+			paintComponents(g);
+			this.repaint();
+		}
+
+
+		class MyMouseAdapter extends MouseAdapter{
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("클릭되었습니다.");
+			}
+		}
+
 
 }
