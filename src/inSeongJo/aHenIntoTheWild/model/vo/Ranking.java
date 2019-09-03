7e8713c3ddcd87bc2533802dbdd4d8feba9f54d2
juhyePC <juhyePC@192.168.0.19>
2019-09-03 18:01:26 +0900

    Ranking Class추가함 (아직 테스트중임)

---
+++ src/inSeongJo/aHenIntoTheWild/model/vo/Ranking.java
@@ -0,0 +1,38 @@
+package inSeongJo.aHenIntoTheWild.model.vo;
+
+public class Ranking {
+	private String name;
+	private int score;
+	
+	public Ranking() {}
+	
+	public Ranking(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "Ranking [name=" + name + ", score=" + score + "]";
+	}
+	
+	
+	
+
+}
